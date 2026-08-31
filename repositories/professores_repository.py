09faa2c450a2dfa04a91a93ffb9a01331f62c332
Daniel Bonin <danielbonin0011@gmail.com>
2026-08-31 11:19:02 -0300

    Adesão do repository e service da tabela professores, sendo assim, finalziamos as tabelas, faltando apenas os relatórios.

--- repositories/professores_repository.py
+++ repositories/professores_repository.py
@@ -1,1 +1,131 @@
 from database.conexao_postgre import conectar
+
+def cadastrar_professor(nome, idade, cpf, especialidade):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+        INSERT INTO professores (nome, idade, cpf, especiaidade)
+        VALUES (%s, %s, %s, %s)
+        """, nome, idade, cpf, especialidade)
+
+        resultado = cursor.rowcount
+
+        if resultado > 0:
+            conexao.commit()
+            return resultado
+
+        conexao.rollback()
+        return 0
+
+    except Exception as erro:
+        conexao.rollback()
+        print(f'Erro ao cadastrar professor: {erro}.')
+        return 0
+
+    finally: 
+        cursor.close()
+        conexao.close()
+
+def listar_professores():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+        SELECT * FROM professores
+        """)
+
+        resultado = cursor.fetchall()
+
+        return resultado
+
+    except Exception as erro:
+        print(f'Erro ao listar professores: {erro}.')
+        return 0
+
+    finally:
+        cursor.close()
+        conexao.close()
+
+def buscar_professor(cpf):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+        SELECT * FROM professores
+        WHERE cpf = %s
+        """, (cpf,))
+
+        resultado = cursor.rowcount
+
+        if resultado > 0:
+            return resultado
+
+        conexao.rollback()
+        return 0
+
+    except Exception as erro:
+        conexao.rollback()
+        print(f'Erro ao buscar professor: {erro}.')
+        return 0
+
+    finally:
+        cursor.close()
+        conexao.close()
+
+def alterar_professor(id_professor, nome, idade, cpf, especialidade):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+        UPDATE professores
+        SET nome = %s, idade = %s, cpf = %s, especialidade = %s
+        WHERE id_aluno = %s
+        """,(nome, idade, cpf, especialidade, id_professor))
+
+        resultado = cursor.rowcount
+
+        if resultado > 0:
+            conexao.commit()
+
+        conexao.rollback()
+        return 0
+
+    except Exception as erro:
+        conexao.rollback()
+        print(f'Erro ao alterar professor: {erro}.')
+        return 0
+
+    finally:
+        cursor.close()
+        conexao.close()
+
+def excluir_professor(cpf):
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+        DELETE FROM professores
+        WHERE cpf = %s
+        """,(cpf,))
+        resultado = cursor.rowcount
+
+        if resultado > 0:
+            conexao.commit()
+
+        conexao.rollback()
+        return 0
+
+    except Exception as erro:
+        conexao.rollback()
+        print(f'Erro ao alterar professor: {erro}.')
+        return 0
+
+    finally:
+        cursor.close()
+        conexao.close()
---
+++ services/professores_service..py
@@ -0,0 +1,101 @@
+from repositories import professores_repository
+
+def cadastrar_professor_service(nome, idade, cpf, especialidade):
+
+    if nome is None or not nome.strip():
+        return 'Preencha o campo "Nome".'
+
+    if idade is None:
+        return 'Preencha o campo "Idade".'
+    
+    if cpf is None or not cpf.strip():
+        return 'Preencha o campo "CPF".'
+    
+    if especialidade is None or not especialidade.strip():
+        return 'Preencha o campo "Especialidade".'
+
+    if idade < 18:
+        return 'Nossos professores devem ser maiores de idade.'
+
+
+    professor = professores_repository.buscar_professor(cpf)
+
+    if professor is not None:
+        return 'O CPF já está cadastrado.'
+
+    resultado = professores_repository.cadastrar_professor(nome, idade, cpf, especialidade)
+
+    if resultado == 0:
+        return 'Não foi possível cadastrar o professor.'
+
+    return resultado 
+
+def listar_professores_service():
+
+    resultado = professores_repository.listar_professores()
+
+    if resultado == 0:
+        return 'Não foi possível listar os professores.'
+
+    return resultado
+
+def buscar_professor_service(cpf):
+
+    if cpf is None or not cpf.strip():
+        return 'Preencha o campo "CPF".'
+
+    resultado = professores_repository.buscar_professor(cpf)
+
+    if resultado == 0:
+        return ('Não foi possível localizar o professor.')
+
+    return resultado
+
+def alterar_professor_service(id_professor, nome, idade, cpf, especialidade):
+
+    if id_professor is None:
+        return 'Preencha o campo "ID Professor".'
+
+    if nome is None or not nome.strip():
+        return 'Preencha o campo "Nome".'
+
+    if idade is None:
+        return 'Preencha o campo "Idade".'
+    
+    if cpf is None or not cpf.strip():
+        return 'Preencha o campo "CPF".'
+    
+    if especialidade is None or not especialidade.strip():
+        return 'Preencha o campo "Especialidade".'
+
+    if idade < 18:
+        return 'Nossos professores devem ser maiores de idade.'
+
+    professor = professores_repository.buscar_professor(cpf)
+
+    if professor == 0:
+        return 'O CPF informado não está vinculado a nenhum professor.'
+
+    resultado = professores_repository.alterar_professor()
+
+    if resultado == 0:
+        return 'Não foi possível alterar as infromações do professor.'
+
+    return resultado
+
+def excluir_professor_service(cpf):
+
+    if cpf is None or not cpf.strip:
+        return 'Preencha o campo "CPF".' 
+
+    professor = professores_repository.buscar_professor(cpf)
+
+    if professor == 0:
+        return 'O CPF informado não está vinculado a nenhum professor.'
+
+    resultado = professores_repository.excluir_professor(cpf)
+
+    if resultado == 0:
+        return 'Erro ao remover professor.'
+    
+    return resultado 
