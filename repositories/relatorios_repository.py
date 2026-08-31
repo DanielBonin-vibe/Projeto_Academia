64c24a94282b95f8f251c7db4bbea05402148364
Daniel Bonin <danielbonin0011@gmail.com>
2026-08-31 13:00:04 -0300

    início da refatoração do repository dos relatórios.

--- repositories/relatorios_repository.py
+++ repositories/relatorios_repository.py
@@ -1,1 +1,21 @@
 from database.conexao_postgre import conectar
+
+def aluno_plano():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    try:
+        cursor.execute("""
+        SELECT id_plano FROM alunos
+        """)
+
+        resultado = cursor.fetchall()
+
+        if resultado:
+            conexao.commit()
+            return resultado
+
+        conexao.r
+
+    except Exception as erro:
+        conexao.rollback()
